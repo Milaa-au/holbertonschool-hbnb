b2f0ddfe1f7e913883c64c6e313ea5a9f034f50e
Milaa-au <mila.audu@icloud.com>
2026-02-27 14:18:01 +0000

    docs: Added descriptions of functions.

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -15,7 +15,24 @@ class AmenityList(Resource):
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
-        """Register a new amenity"""
+
+        """
+        Create a new amenity.
+
+        This method retrieves the JSON data sent in the
+        request and calls the facade layer to create a
+        new amenity.
+
+        Returns:
+        tuple:
+            - dict: Data for the created amenity (id, name).
+            - int: HTTP code 201 if successful.
+
+        If the data is invalid:
+            - dict: Error message.
+            - int: HTTP code 400.
+        """
+
         amenity_data = api.payload
         amenity_new = facade.create_amenity(amenity_data)
         if not new_amenity:
@@ -24,7 +41,19 @@ class AmenityList(Resource):
 
     @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
-        """Retrieve a list of all amenities"""
+
+        """
+        Retrieve the list of all amenities.
+
+        This method queries the facade layer to
+        retrieve all registered amenities.
+
+        Returns:
+        tuple:
+            - list: List of amenities in dictionary format.
+            - int: HTTP code 200 if successful.
+        """
+
         amenities = get_all_amenities()
         return [amenity.to_dict() for amenity in amenities], 200
 
@@ -33,7 +62,26 @@ class AmenityResource(Resource):
     @api.response(200, 'Amenity details retrieved successfully')
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
-        """Get amenity details by ID"""
+
+        """
+        Retrieve the details of an amenity by its ID.
+
+        This method searches for a specific amenity
+        using its ID via the facade layer.
+
+        Args:
+        amenity_id (str): Unique ID of the amenity.
+
+        Returns:
+        tuple:
+            - dict: The amenity data if it exists.
+            - int: HTTP code 200 if successful.
+
+        If the amenity does not exist:
+            - dict: Error message.
+            - int: HTTP code 404.
+        """
+
         try:
             amenity = get_amenity(amenity_id)
         except ValueError:
@@ -46,7 +94,30 @@ class AmenityResource(Resource):
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
-        """Update an amenity's information"""
+
+        """
+        Update amenity information.
+
+        This method updates an existing amenity
+        based on the JSON data received in the request.
+
+        Args:
+        amenity_id (str): Unique identifier for the amenity.
+
+        Returns:
+        tuple:
+            - dict: Updated amenity data.
+            - int: HTTP code 200 if successful.
+
+        If the amenity does not exist:
+            - dict: Error message.
+            - int: HTTP code 404.
+
+        If the data is invalid:
+            - dict: Error message.
+            - int: HTTP code 400.
+        """
+
         data = request.get_json()
 
         if not data:
