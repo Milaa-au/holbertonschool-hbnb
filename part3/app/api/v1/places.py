6ea5946e67f9748bdbebaec4033059e4cc4bfcf8
Milaa-au <mila.audu@icloud.com>
2026-03-09 13:55:33 +0000

    Feat: Add part3

---
+++ part3/app/api/v1/places.py
@@ -0,0 +1,213 @@
+"""
+User API namespace.
+
+This module defines REST endpoints for managing places, including
+creation, retrieval, and update operations. It relies on a facade
+service layer to handle business logic and data persistence.
+"""
+
+
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('places', description='Place operations')
+
+# Define the models for related entities
+amenity_model = api.model('PlaceAmenity', {
+    'id': fields.String(description='Amenity ID'),
+    'name': fields.String(description='Name of the amenity')
+})
+
+user_model = api.model('PlaceUser', {
+    'id': fields.String(description='User ID'),
+    'first_name': fields.String(description='First name of the owner'),
+    'last_name': fields.String(description='Last name of the owner'),
+    'email': fields.String(description='Email of the owner')
+})
+
+# Define the place model for input validation and documentation
+place_model = api.model('Place', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(
+        required=True,
+        description='Latitude of the place'
+    ),
+    'longitude': fields.Float(
+        required=True,
+        description='Longitude of the place'
+    ),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'amenities': fields.List(
+        fields.String, required=True,
+        description="List of amenities ID's"
+    )
+})
+
+review_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
+
+place_model = api.model('Place', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'owner': fields.Nested(user_model, description='Owner of the place'),
+    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
+    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
+})
+
+@api.route('/')
+class PlaceList(Resource):
+    """
+    Resource for operations on the place collection.
+
+    Provides endpoints to create a new place and to retrieve
+    the list of all places.
+    """
+    @api.expect(place_model)
+    @api.response(201, 'Place successfully created')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        """
+        Register a new place.
+
+        Retrieves the JSON payload from the API request and attempts to create
+        a new place using the facade. If creation fails due to invalid data,
+        returns a 400 response with an error message.
+
+        Returns:
+        tuple: A JSON dictionary containing the new place details and
+        the HTTP status code 201 if successful, or an error message and
+        status code 400 if creation fails.
+        """
+        place_data = api.payload
+
+        try:
+            new_place = facade.create_place(place_data)
+        except ValueError as error:
+            return {'error': str(error)}, 400
+        return {
+            'id': new_place.id,
+            'title': new_place.title,
+            'description': new_place.description,
+            'price': new_place.price,
+            'latitude': new_place.latitude,
+            'longitude': new_place.longitude,
+            'owner_id': new_place.owner.id
+        }, 201
+
+    @api.response(200, 'List of places retrieved successfully')
+    def get(self):
+        """
+        Retrieve the list of all locations.
+
+        Retrieves all locations via the facade and returns a simplified list
+        containing the ID, title, latitude, and longitude of each location.
+
+        Returns:
+        tuple: A list of dictionaries representing each location and
+        the HTTP 200 code.
+        """
+        places = facade.get_all_places()
+        List_places = []
+        for place in places:
+            List_places.append({
+                'id': place.id,
+                'title': place.title,
+                'latitude': place.latitude,
+                'longitude': place.longitude
+            })
+        return List_places, 200
+
+
+@api.route('/<place_id>')
+class PlaceResource(Resource):
+    @api.response(200, 'Place details retrieved successfully')
+    @api.response(404, 'Place not found')
+    def get(self, place_id):
+        """
+        Retrieve details of a place by its ID.
+
+        Retrieves a specific place via its ID. If the place does not exist,
+        returns a 404 error with an appropriate message.
+
+        Args:
+        place_id (str): The unique identifier of the place to retrieve.
+
+        Returns:
+        tuple: A dictionary containing detailed information
+        about the place and HTTP code 200 if found, or an error message
+        with code 404 if not found."""
+        place = facade.get_place(place_id)
+        if not place:
+            return {'error': 'Place not found'}, 404
+        return {
+            'id': place.id,
+            'title': place.title,
+            'description': place.description,
+            'price': place.price,
+            'latitude': place.latitude,
+            'longitude': place.longitude,
+            'owner': {
+                'id': place.owner.id,
+                'first_name': place.owner.first_name,
+                'last_name': place.owner.last_name,
+                'email': place.owner.email
+            },
+            'amenities': [
+                {
+                    'id': amenity.id,
+                    'name': amenity.name
+                }
+                for amenity in place.amenities
+            ]
+        }, 200
+
+    @api.expect(place_model)
+    @api.response(200, 'Place updated successfully')
+    @api.response(404, 'Place not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, place_id):
+        """
+        Update place information.
+
+        Accepts JSON data from the request and attempts to update
+        an existing place identified by place_id. If the data is invalid,
+        returns a 400 response. If the place does not exist, returns a 404 response.
+
+        Args:
+        place_id (str): The unique identifier of the place to update.
+
+        Returns:
+        tuple: A dictionary containing the updated information
+        for the place and HTTP code 200 if successful, or an error message
+        with code 400 or 404 if unsuccessful.
+        """
+        place_data = api.payload
+
+        try:
+            update_place = facade.update_place(place_id, place_data)
+        except ValueError as error:
+            return {'error': str(error)}, 400
+
+        if not update_place:
+            return {'error': 'Place not found'}, 404
+
+        return {
+            'id': update_place.id,
+            'title': update_place.title,
+            'description': update_place.description,
+            'price': update_place.price,
+            'latitude': update_place.latitude,
+            'longitude': update_place.longitude,
+            'owner_id': update_place.owner.id
+        }, 200
+
---
+++ part3/app/services/facade.py
@@ -0,0 +1,376 @@
+"""Facade is an intermediary between the API layer and
+the persistance layer.
+"""
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.place import Place
+from app.models.amenity import Amenity
+from app.models.review import Review
+
+
+class HBnBFacade:
+    """HBnBFacade acts as an intermediary between the API layer and the
+        persistence layer. It centralizes business logic and abstracts
+        direct access to the repositories.
+    """
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        """
+        Creates a new user.
+
+        Args:
+        user_data (dict): Dictionary containing user information.
+
+        Returns:
+        User: The created user object.
+        """
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        """
+        Retrieves a user by their ID.
+
+        Args:
+        user_id (str): User ID.
+
+        Returns:
+        User: The corresponding user object or None if not found.
+        """
+        return self.user_repo.get(user_id)
+
+    def get_all_users(self):
+        """
+        Retrieves all users.
+
+        Returns:
+        list: List of all user objects.
+        """
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        """
+        Updates the information for an existing user.
+
+        Args:
+        user_id (str): ID of the user to update.
+        user_data (dict): Dictionary of fields to update.
+
+        Returns:
+        User: The updated user object, or None if the user does not exist.
+        """
+        user = self.get_user(user_id)
+        if not user:
+            return None
+        self.user_repo.update(user_id, user_data)
+        return self.get_user(user_id)
+
+    def get_user_by_email(self, email):
+        """
+        Retrieves a user from their email address.
+
+        Args:
+        email (str): The user's email address.
+
+        Returns:
+        User: The corresponding user object or None if not found.
+        """
+        return self.user_repo.get_by_attribute('email', email)
+
+    def create_place(self, place_data):
+        """
+        Creates a new place with validation of required fields.
+
+        Args:
+        place_data (dict): Dictionary containing the place information.
+
+        Raises:
+        ValueError: If a required field is missing or invalid.
+
+        Returns:
+        Place: The created place object.
+        """
+        try:
+            owner_id = place_data["owner_id"]
+            title = place_data["title"]
+            description = place_data.get("description")
+            price = place_data["price"]
+            latitude = place_data["latitude"]
+            longitude = place_data["longitude"]
+            amenities_ids = place_data.get("amenities", [])
+        except KeyError as e:
+            raise ValueError(f"Missing field: {str(e)}")
+
+        owner = self.get_user(owner_id)
+        if not owner:
+            raise ValueError("Owner not found")
+
+        place = Place(
+            title,
+            description,
+            price,
+            latitude,
+            longitude,
+            owner
+        )
+
+        for amenity_id in amenities_ids:
+            amenity = self.amenity_repo.get(amenity_id)
+            if not amenity:
+                raise ValueError("Amenity not found")
+            place.add_amenity(amenity)
+
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        """
+            Retrieves a place by its identifier.
+
+        Args:
+        place_id (str): Identifier of the place.
+
+        Returns:
+        Place: The corresponding place object or None if not found.
+        """
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        """
+        Retrieves all existing locations.
+
+        Returns:
+        list: List of all location objects.
+        """
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        """
+        Updates an existing place with validation of modified fields.
+
+        Args:
+        place_id (str): ID of the place to be updated.
+        place_data (dict): Dictionary containing the fields to be updated.
+
+        Raises:
+        ValueError: If a value is invalid (price <= 0, 
+        latitude/longitude out of bounds, invalid title).
+
+        Returns:
+        Place: The updated place object or None if the place does not exist.
+        """
+        place = self.get_place(place_id)
+        if not place:
+            return None
+
+        place_data.pop("owner_id", None)
+        place_data.pop("amenities", None)
+
+        if "price" in place_data and place_data["price"] <= 0:
+            raise ValueError("Price must be greater than 0")
+
+        if "latitude" in place_data:
+            if place_data["latitude"] < -90 or place_data["latitude"] > 90:
+                raise ValueError("Latitude must be between -90 and 90")
+
+        if "longitude" in place_data:
+            if place_data["longitude"] < -180 or place_data["longitude"] > 180:
+                raise ValueError("Longitude must be between -180 and 180")
+
+        if "title" in place_data:
+            if not place_data["title"] or len(place_data["title"]) > 100:
+                raise ValueError("Invalid title")
+
+        self.place_repo.update(place_id, place_data)
+        return self.get_place(place_id)
+
+    def create_amenity(self, amenity_data):
+        """
+        Creates a new Amenity instance after
+        validating the data provided.
+
+        This method checks that the
+        data entered is valid and contains the
+        required fields before creating
+        a new Amenity object. If the data
+        is invalid or if required information
+        is missing, a ValueError error is raised.
+
+        Once validated, the Amenity instance
+        is instantiated and stored in the
+        internal amenities collection, then returned.
+        """
+
+        if not amenity_data or not isinstance(amenity_data, dict):
+            raise ValueError("Invalid amenity data")
+        if "name" not in amenity_data or not amenity_data["name"]:
+            raise ValueError("Amenity name is required")
+            
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+
+        """
+        Retrieves a commodity from its identifier.
+
+        Verifies that the identifier is valid and exists in
+        memory storage before returning the corresponding object.
+        """
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """
+        Retrieves all amenities stored in the database.
+
+        Performs a query to return all
+        Amenity objects present in the database.
+        """
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """
+        Updates an existing convenience.
+
+        Searches for the convenience by its identifier, updates its
+        attributes with the provided data, then saves the
+        changes to the database.
+        """
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            return None
+
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.get_amenity(amenity_id)
+
+    def create_review(self, review_data):
+        """
+        Create a new review after validating the data.
+
+        This method validates the data provided, verifies
+        the existence of the associated user and location,
+        then creates and saves a new review.
+
+        Args:
+        review_data (dict): Review data containing
+            text, rating, user_id, and place_id.
+
+        Returns:
+        Review: Instance of the review created.
+
+        Raises:
+        ValueError: If the data is invalid, if a required field
+        is missing, or if the user or place
+        does not exist.
+        """
+        user = self.user_repo.get(review_data['user_id'])
+        if not user:
+            raise KeyError('Invalid input data')
+        
+        place = self.place_repo.get(review_data['place_id'])
+        if not place:
+            raise KeyError('Invalid input data')
+
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        user.add_review(review)
+        place.add_review(review)
+        return review
+
+    def get_review(self, review_id):
+        """
+        Retrieve a review by its ID.
+
+        Args:
+        review_id (int): Unique ID of the review.
+
+        Returns:
+        Review: Instance corresponding to the ID provided.
+
+        Raises:
+        ValueError: If the ID is invalid or if the
+        review does not exist.
+        """
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        """
+        Retrieve all saved reviews.
+
+        Returns:
+        list: List of all Review instances.
+        """
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        """
+        Retrieve all reviews associated with a given location.
+
+        Args:
+        place_id (str): Unique identifier for the location.
+
+        Returns:
+        list: List of reviews associated with the location.
+
+        Raises:
+        ValueError: If the location identifier is invalid
+        or if the location does not exist.
+        """
+        place = self.place_repo.get(place_id)
+        if not place:
+            raise KeyError('Place not found')
+        return place.reviews
+
+    def update_review(self, review_id, review_data):
+        """
+        Update an existing review.
+
+        This method updates only the authorized fields
+        of a review, then saves the changes to the database.
+    
+
+        Args:
+        review_id (int): Unique identifier of the review.
+        review_data (dict): Data to be modified.
+
+        Returns:
+        Review: Updated instance.
+
+        Raises:
+        ValueError: If the review does not exist.
+        """    
+        review = self.get_review(review_id)
+        if not review:
+            return None
+
+        self.review_repo.update(review_id, review_data)
+        return self.get_review(review_id)
+
+    def delete_review(self, review_id):
+        """
+        Delete an existing review.
+
+        Args:
+        review_id (int): Unique identifier of the review.
+
+        Returns:
+        bool: True if the deletion is successful.
+
+        Raises:
+        ValueError: If the review does not exist.
+        """
+        review = self.review_repo.get(review_id)
+        
+        user = self.user_repo.get(review.user.id)
+        place = self.place_repo.get(review.place.id)
+
+        user.delete_review(review)
+        place.delete_review(review)
+        self.review_repo.delete(review_id)
