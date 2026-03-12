b675508a009a999d389c9a37e19dbf9eadda65fa
Milaa-au <mila.audu@icloud.com>
2026-03-12 13:10:07 +0000

    Update: change class User and add validates.

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,61 +1,70 @@
 #!/usr/bin/python3
-"""
-User model.
-
-This module defines the User entity which extends BaseModel and
-represents an application user with validation rules for core fields.
-"""
-
-
-from app.models.base_model import BaseModel
-import re
-
+from app import db, bcrypt
+import uuid
+from .base_model import BaseModel 
 
 class User(BaseModel):
-    """
-    User domain model.
+     __tablename__ = 'users'
 
-    Represents a user in the system with basic identity information
-    and optional administrative privileges. Validation is performed
-    during initialization to ensure data integrity.
-    """
+     first_name = db.Column(db.String(50), nullable=False)
+     last_name = db.Column(db.String(50), nullable=False)
+     email = db.Column(db.String(120), nullable=False, unique=True)
+     password = db.Column(db.String(128), nullable=False)
+     is_admin = db.Column(db.Boolean, default=False)
 
-    def __init__(self, first_name, last_name, email, is_admin=False):
-        """
-        Initialize a new User instance.
+    def hash_password(self, password):
+        """Hash the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-        Validates required fields and constraints such as maximum
-        length for names and basic email format.
+    def verify_password(self, password):
+        """Verify the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
 
-        Args:
-            first_name (str): User's first name (required, max 50 characters).
-            last_name (str): User's last name (required, max 50 characters).
-            email (str): User's email address (required, must be valid format).
-            is_admin (bool, optional): Indicates whether the user has
-                administrative privileges. Defaults to False.
-
-        Raises:
-            ValueError: If any required field is missing or invalid.
-        """
-        super().__init__()
-        if not first_name:
+    @validates('first_name')
+    def validate_first_name(self, key, value):
+        if not isinstance(value, str) or not value.strip():
             raise ValueError("First name is required")
-        if len(first_name) > 50:
+        if len(value) > 50:
             raise ValueError("First name must be 50 characters or less")
-        self.first_name = first_name
+        self.__first_name = value
+        return value
 
-        if not last_name:
+    @validates('last_name')
+    def validate_last_name(self, key, value):
+        if not isinstance(value, str) or not value.strip():
             raise ValueError("Last name is required")
-        if len(last_name) > 50:
+        if len(value) > 50:
             raise ValueError("Last name must be 50 characters or less")
-        self.last_name = last_name
-
-        if not email:
+        self.__last_name = value
+        return value
+    
+    @validates('email')
+    def validate_email(self, key, value):
+        if not isinstance(value, str):
             raise ValueError("Email is required")
         pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
-        if not re.match(pattern, email):
+        if not re.match(pattern, value):
             raise ValueError("Invalid email format")
-        self.email = email
 
-        self.is_admin = is_admin
+        self.__email = value
+        return value
+
+    @validates('is_admin')
+    def validate_is_admin(self, key, value):
+        if not isinstance(value, bool):
+            raise TypeError("Is Admin must be a boolean")
+        self.__is_admin = value
+        return value
+
+    def add_place(self, place):
+        """Add an amenity to the place."""
+        self.places.append(place)
+
+    def add_review(self, review):
+        """Add an amenity to the place."""
+        self.reviews.append(review)
+
+    def delete_review(self, review):
+        """Add an amenity to the place."""
+        self.reviews.remove(review)
